3ade328c269961a3246d491671cb44d443a2c052
thigo47 <vilaportilho@gmail.com>
2023-05-15 23:17:53 +0100

    att

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router , Route , Link } from 'react-router-dom';
+import styles from "./estilo.module.css";
+import { useState } from 'react';
+
 
 function App() {
+  const [valorq1 , setValorq1] = useState("")
+  const [valorq2 , setValorq2] = useState("")
+  const [valorq3 , setValorq3] = useState("")
+  const [valorq4 , setValorq4] = useState("")
+  const [valorq5 , setValorq5] = useState("")
+  const [valorq6 , setValorq6] = useState("")
+  const [valorq7 , setValorq7] = useState("")
+  const [valorq8 , setValorq8] = useState("")
+  const [valorq9 , setValorq9] = useState("")
+
+  function clicouq1(params) {
+    setValorq1("X")
+    setInterval(()=>{
+      setValorq2("O")
+    }, 1000)
+   
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={styles.container}>
+
+      <div className={styles.q1} onClick={clicouq1}>{valorq1}</div>
+      
+      <div className={styles.q2}>{valorq2}</div>
+      
+      <div className={styles.q3}>{valorq3}</div>
+      
+      
+      <div className={styles.q4}>{valorq4}</div>
+      
+      <div className={styles.q5}>{valorq5}</div>
+
+      <div className={styles.q6}>{valorq6}</div>
+
+      <div className={styles.q7}>{valorq7}</div>
+
+      <div className={styles.q8}>{valorq8}</div>
+
+      <div className={styles.q9}>{valorq9}</div>
+
+
+
+      </div>
     </div>
   );
 }
